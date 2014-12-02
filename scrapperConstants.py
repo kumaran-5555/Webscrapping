11dff41cfce9970879d0e1265d3928b7fbf86399
serajago <kumaran_5555@outlook.com>
2014-12-02 08:44:54 +0530

    lniked list v0

---
+++ scrapperConstants.py
@@ -0,0 +1,29 @@
+
+JobNew = 0
+JobSuccess = 1
+JobIncomplete = 2
+JobTimeout = 3
+JobError = 4
+
+JobStatusIntStr = ["NEW", "SUCCESS", "INCOMPLETE", "TIMEOUT", "ERROR"]
+JobStatusStrInt = {"NEW":JobNew, "SUCCESS":JobSuccess, "INCOMPLETE":JobIncomplete,\
+ "TIMEOUT":JobTimeout, "ERROR":JobError}
+
+SlaveStatusReady = 10
+SlaveStatusDone	= 11
+
+
+MasterCmdJob = 0
+MasterCmdExit = 1
+
+
+LogDebug = 0
+LogInfo = 1
+LogWarn	= 2
+LogError = 3
+LogCritical = 4
+LogLevels = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL']
+
+
+__author__ = 'serajago'
+__author__ = 'serajago'
