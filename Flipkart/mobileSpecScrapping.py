bb3b7d96e553006f5ad5c98e776f41cedb98b6c2
serajago <kumaran_5555@outlook.com>
2014-12-03 23:25:21 +0530

    specs v0

--- Flipkart/mobileSpecScrapping.py
+++ Flipkart/mobileSpecScrapping.py
@@ -1,7 +1,9 @@
 __author__ = 'serajago'
 
 import webkitBrowser
+import sys
 
+baseUrl = "http://www.flipkart.com"
 productsLinkDataFile = open("productsLinkData.tsv", "r", encoding="utf-8")
 productsSpecScrapeStsFile = open("productsSpecScrapeStsFile.tsv", "r", encoding="utf-8")
 productsSpecDataFile = open("productsSpecDataFile.tsv", "a", encoding="utf-8")
@@ -10,11 +12,12 @@ productsSpecScrapeSts = []
 productsLinkData = []
 
 for l in productsLinkDataFile:
-    productsLinkData.append(l.split("\t")[1])
+    productsLinkData.append(l[:-1].split("\t")[1])
 
 for l in productsSpecScrapeStsFile:
     if l[:-1].split("\t") == "DONE":
         productsSpecScrapeSts.append(l[:-1].split("\t")[0])
+
 productsSpecScrapeStsFile.close()
 productsSpecScrapeStsFile = open("productsSpecScrapeStsFile.tsv", "w", encoding="utf-8")
 
@@ -22,11 +25,23 @@ for product in productsSpecScrapeSts:
     productsSpecScrapeStsFile.write(product + "\tDONE")
 productsSpecScrapeStsFile.flush()
 
+print(productsLinkData)
+print(productsSpecScrapeSts)
+
 b = webkitBrowser.Browser(gui=True, loadImages=False)
 
 for product in productsLinkData:
-    if product in productsLinkData:
+    if product in productsSpecScrapeSts:
         print("sts: Skipping product " + product[0:25] + "...")
         continue
-    print("sts: Processing product "+ product[0:25])
-    b.loadPage(b.decodedUrl(product))
+    print("sts: Processing product " + product[0:25])
+    b.loadPage(b.decodedUrl(baseUrl + product))
+    b.waitSecs(5)
+    for t in b.findElement('table[class="specTable"]>tbody'):
+        tableHeader = t.findAll('tr>th')[0].toInnerXml().strip().lower().replace(" ", "_")
+        for td in t.findAll('tr>td'):
+            if td.attribute("class")=="specsKey":
+                print(tableHeader+td.toInnerXml().strip())
+            else:
+                print(td.toInnerXml().strip())
+    break
