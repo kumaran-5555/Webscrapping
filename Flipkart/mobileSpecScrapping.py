0a29dd748cd97fef3f551f1ff07d12680a01ede1
serajago <kumaran_5555@outlook.com>
2014-12-06 15:52:50 +0530

    flipkart specs done

--- Flipkart/mobileSpecScrapping.py
+++ Flipkart/mobileSpecScrapping.py
@@ -15,33 +15,78 @@ for l in productsLinkDataFile:
     productsLinkData.append(l[:-1].split("\t")[1])
 
 for l in productsSpecScrapeStsFile:
-    if l[:-1].split("\t") == "DONE":
+    if l[:-1].split("\t")[1] == "DONE":
         productsSpecScrapeSts.append(l[:-1].split("\t")[0])
 
 productsSpecScrapeStsFile.close()
 productsSpecScrapeStsFile = open("productsSpecScrapeStsFile.tsv", "w", encoding="utf-8")
 
 for product in productsSpecScrapeSts:
-    productsSpecScrapeStsFile.write(product + "\tDONE")
+    productsSpecScrapeStsFile.write(product + "\tDONE\n")
 productsSpecScrapeStsFile.flush()
 
 print(productsLinkData)
 print(productsSpecScrapeSts)
 
-b = webkitBrowser.Browser(gui=True, loadImages=False)
-
+b = webkitBrowser.Browser(gui=True, loadImages=False, javaScriptEnabled=False)
+productCount = 0
 for product in productsLinkData:
+    productCount += 1
     if product in productsSpecScrapeSts:
         print("sts: Skipping product " + product[0:25] + "...")
         continue
     print("sts: Processing product " + product[0:25])
+    # clear the content for reset and error anding
+    b.setHtml('<html><head></head><body>No content loaded</body></html>')
     b.loadPage(b.decodedUrl(baseUrl + product))
-    b.waitSecs(5)
+    outputStr = ""
+
+    if len(b.findElement('span[class="selling-price omniture-field"]')) > 0:
+        outputStr = outputStr + "OfficePrice|" + b.findElement('span[class="selling-price omniture-field"]')[
+            0].toInnerXml()
+    else:
+        outputStr = outputStr + "OfficePrice|"
+
+    if len(b.findElement('div[class="prices"]>span[class="price"]')) > 0:
+        outputStr = outputStr + "\t" + "mrpPrice|" + b.findElement('div[class="prices"]>span[class="price"]')[
+            0].toInnerXml()
+    else:
+        outputStr = outputStr + "\t" + "mrpPrice|"
+
+    if len(b.findElement(
+        'div[class="title-wrap line fk-font-family-museo section omniture-field"]>h1[class="title"]')) >0:
+        outputStr = outputStr + "\t" + "name|" + b.findElement(
+            'div[class="title-wrap line fk-font-family-museo section omniture-field"]>h1[class="title"]')[ \
+            0].toInnerXml()
+    else:
+        outputStr = outputStr + "\t" + "name|"
+
+    if len(b.findElement(
+            'div[class="title-wrap line fk-font-family-museo section omniture-field"]>span[class="subtitle"]')) > 0:
+        outputStr = outputStr + "\t" + "color|" + b.findElement(
+            'div[class="title-wrap line fk-font-family-museo section omniture-field"]>span[class="subtitle"]')[ \
+            0].toInnerXml()
+    else:
+        outputStr = outputStr + "\t" + "color|"
+
+    specStr = ""
+    # spec scrapping
     for t in b.findElement('table[class="specTable"]>tbody'):
-        tableHeader = t.findAll('tr>th')[0].toInnerXml().strip().lower().replace(" ", "_")
+        specHeader = t.findAll('tr>th')[0].toInnerXml().strip().lower()
         for td in t.findAll('tr>td'):
-            if td.attribute("class")=="specsKey":
-                print(tableHeader+td.toInnerXml().strip())
+            if td.attribute("class") == "specsKey":
+                specKey = td.toInnerXml().strip()
             else:
-                print(td.toInnerXml().strip())
-    break
+                specVal = td.toInnerXml().strip().replace("\n","")
+                specStr = specStr + specHeader + "|" + specKey + "|" + specVal + "|"
+    if specStr != "":
+        productsSpecDataFile.write(product + "\t" + outputStr +"\t" + "spec|" + specStr + "\n")
+        productsSpecScrapeStsFile.write(product + "\tDONE\n")
+    else:
+        productsSpecScrapeStsFile.write(product + "\tFAILED\n")
+
+    productsSpecScrapeStsFile.flush()
+    productsSpecDataFile.flush()
+    print("sts: done product " + product + " total " + str(productCount))
+productsSpecDataFile.close()
+productsSpecScrapeStsFile.close()
